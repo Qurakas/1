0919add216444a802c08ae573fff556f9f0d592d
Qurakas <41931129+Qurakas@users.noreply.github.com>
2018-08-08 11:51:32 -0500

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,6 +21,38 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "qurakas.contacto@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_adress";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+// mail($to,$email_subject,$email_body,$headers);
+// Use sendgrid to send the email
+$params = array(
+    'api_user'  => "Qurakas",
+    'api_key'   => "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",
+    'to'        => "$to",
+    'subject'   => "Contact Form Submission",
+    'html'      => "<html><head><title> Contact Form</title><body>
+    Name: $name\n<br>
+    Email: $email\n<br>
+    Subject: $subject\n<br>
+    Message: $message </body></head></html>",
+    'text'      => "
+    Name: $name\n
+    Email: $email\n
+    Subject: $subject\n
+    $message",
+    'from'      => "qurakas.contacto@gmail.com",
+);
+$request = 'https://api.sendgrid.com/api/mail.send.json'
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+return true;
 ?>
+
